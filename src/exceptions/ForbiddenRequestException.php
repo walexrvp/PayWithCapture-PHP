6de05199f39b14ac99c08963d970f5d3ca3bb875
Adeoye Remi <mail4remi@gmail.com>
2016-08-09 14:47:01 +0100

    Fixed 'Exception' namespace in errors

--- src/exceptions/ForbiddenRequestException.php
+++ src/exceptions/ForbiddenRequestException.php
@@ -1,6 +1,8 @@
 <?php
 namespace PayWithCapture\Errors;
 
+use Exception;
+
 class ForbiddenRequestException extends Exception{
   public function __construct($message, $code = 0, Exception $previous = null) {
     parent::__construct($message, $code, $previous);
